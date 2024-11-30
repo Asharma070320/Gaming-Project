3bd4cdc3f007cc292f81af68b9b2402fedcedc46
AmanAsharma070320 <asharma070320@gmail.com>
2024-12-01 01:03:27 +0530

    Gaming Project

--- src/components/Sections/LeftSection.js
+++ src/components/Sections/LeftSection.js
@@ -2,28 +2,31 @@ import React, { useState } from "react";
 import "./LeftSection.css";
 
 const LeftSection = () => {
-  const [showRacingList, setShowRacingList] = useState(false); // State for Racing Sports list
-  const [showOthersList, setShowOthersList] = useState(false); // State for Others list
-  const [showAllSportsList, setShowAllSportsList] = useState(false); // State for All Sports list
-  const[game,setGame] = useState(false);
+  const [showRacingList, setShowRacingList] = useState(true); 
+  const [showOthersList, setShowOthersList] = useState(true); 
+  const [showAllSportsList, setShowAllSportsList] = useState(true); 
+  const [game, setGame] = useState(false);
+  const[assembly,setAssembly] = useState(false)
 
   const toggleRacingList = () => {
-    setShowRacingList((prevState) => !prevState); // Toggle Racing Sports list
+    setShowRacingList((prevState) => !prevState); 
   };
 
   const toggleOthersList = () => {
-    setShowOthersList((prevState) => !prevState); // Toggle Others list
+    setShowOthersList((prevState) => !prevState); 
   };
 
   const toggleAllSportsList = () => {
-    setShowAllSportsList((prevState) => !prevState); // Toggle All Sports list
+    setShowAllSportsList((prevState) => !prevState); 
   };
 
   const toggleGame = () => {
-    setGame((prevState) => !prevState)
+    setGame((prevState) => !prevState);
+  };
+
+  const assemblyBtn = () => {
+    setAssembly((prevState) => !prevState)
   }
-  console.log(game);
-  
 
   return (
     <div className="leftSection_container">
@@ -38,13 +41,9 @@ const LeftSection = () => {
       </div>
       <div className={`show_list ${showRacingList ? "show_list_visible" : ""}`}>
         <ul className="list_items">
-          <li className="shift">
-             Horse Racing
-          </li>
+          <li className="shift">Horse Racing</li>
           <div className="line"></div>
-          <li className="shift">
-             Greyhound Racing
-          </li>
+          <li className="shift">Greyhound Racing</li>
         </ul>
       </div>
 
@@ -84,21 +83,252 @@ const LeftSection = () => {
         className={`show_list ${showAllSportsList ? "show_list_visible" : ""}`}
       >
         <ul className="list_items">
-          <li onClick={toggleGame} className="shift" >
-            <i className="ri-add-box-line add_box"></i> Politics
+          {/* Politics  */}
+          <li onClick={toggleGame} className="shift">
+            <i
+              className={
+                game
+                  ? "ri-checkbox-indeterminate-line cancel_box"
+                  : "ri-add-box-line add_box"
+              }
+            ></i>{" "}
+            Politics
           </li>
+          
+          {game && (
+            <div className="border_top">
+              <li onClick={assemblyBtn} className="shift">
+              <span className="set_txt">
+              <i
+              className={
+                assembly
+                  ? "ri-checkbox-indeterminate-line cancel_box"
+                  : "ri-add-box-line add_box"
+              }
+            ></i>{" "} Assembly Election 2024</span>
+              </li>
+              { assembly && <div className="border_top">
+                <li onClick={assemblyBtn} className="shift">
+                <span style={{paddingLeft:'1.5rem'}} className="set_txt">
+                Assembly Election
+                2024 </span>
+              </li>
+              </div>
+              
+              }
+            </div>
+          )}
+
+          {/* Cricket */}
           <div className="line"></div>
           <li className="shift">
             <i className="ri-add-box-line add_box"></i> Cricket
           </li>
+
+          {/* Football */}
           <div className="line"></div>
           <li className="shift">
             <i className="ri-add-box-line add_box"></i> Football
           </li>
+
+          {/* Tennis */}
           <div className="line"></div>
           <li className="shift">
             <i className="ri-add-box-line add_box"></i> Tennis
           </li>
+
+          {/* Table Tennis */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Table Tennis
+          </li>
+
+          {/* Badminton */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Badminton
+          </li>
+
+          {/* Esoccer */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Esoccer
+          </li>
+
+          {/* Basketball */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Basketball
+          </li>
+
+          {/* Volleyball */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Volleyball
+          </li>
+
+          {/* Snooker */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Snooker
+          </li>
+
+          {/* Ice Hockey */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Ice Hockey
+          </li>
+
+          {/* E Games */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> E Games
+          </li>
+
+          {/* Futsal */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Futsal
+          </li>
+
+          {/* Handball */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Handball
+          </li>
+
+          {/* Kabaddi */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Kabaddi
+          </li>
+
+          {/* Golf */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Golf
+          </li>
+
+          {/* Rugby League */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Rugby League
+          </li>
+
+          {/* Boxing */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Boxing
+          </li>
+
+          {/* Beach Volleyball */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Beach Volleyball
+          </li>
+
+          {/* Mixed Martial Arts */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Mixed Martial Arts
+          </li>
+
+          {/* MotoGP */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> MotoGP
+          </li>
+
+          {/* Chess */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Chess
+          </li>
+
+          {/* Cycling */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Cycling
+          </li>
+
+          {/* Motorbikes */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Motorbikes
+          </li>
+
+          {/* Athletics */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Athletics
+          </li>
+
+          {/* Basketball 3X3 */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Basketball 3X3
+          </li>
+
+          {/* Sumo */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Sumo
+          </li>
+
+          {/* Virtual sports */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Virtual sports
+          </li>
+
+          {/* Motor Sports */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Motor Sports
+          </li>
+
+          {/* Baseball */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Baseball
+          </li>
+
+          {/* Rugby Union */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Rugby Union
+          </li>
+
+          {/* Darts */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Darts
+          </li>
+
+          {/* American Football */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> American Football
+          </li>
+
+          {/* Soccer */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Soccer
+          </li>
+
+          {/* Esports */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Esports
+          </li>
+
+          {/* Boat Racing */}
+          <div className="line"></div>
+          <li className="shift">
+            <i className="ri-add-box-line add_box"></i> Boat Racing
+          </li>
+
         </ul>
       </div>
     </div>
--- src/components/Sections/RightSection.js
+++ src/components/Sections/RightSection.js
@@ -4,7 +4,60 @@ import './RightSection.css'
 const RightSection = () => {
   return (
     <div className='rightSection_container'>
-      <h1>Right Section</h1>
+      <div className="right_highlights">
+        <div className='highlight_box'>
+          <a className='blink_me' href="">
+          <p>New Zealand v England</p>
+          </a>
+        </div>
+        <div className='highlight_box'>
+          <a className='blink_me' href="">
+            <p>South Africa Women v Eng...</p>
+          </a>
+        </div>
+        <div className='highlight_box'>
+          <a className='blink_me' href="">
+            <p>AC Milan v Empoli</p>
+          </a>
+        </div>
+        <div className='highlight_box'>
+          <a className='blink_me' href="">
+            <p>West Ham v Arsenal</p>
+          </a>
+        </div>
+        <div className='highlight_box'>
+          <a className='blink_me' href="">
+            <p>Dortmund v Bayern Munich</p>
+          </a>
+        </div>
+      </div>
+
+      {/* games lists bar */}
+      <div className="game_bar">
+        <ul className="boxes">
+          <li>Politics</li>
+          <li>Cricket</li>
+          <li>Football</li>
+          <li>Tennis</li>
+          <li>Kabaddi</li>
+          <li>Esoccer</li>
+          <li>Horse Racing</li>
+          <li>Greyhound Racing</li>
+          <li>Table Tennis</li>
+          <li>Basketball</li>
+          <li>Boxing</li>
+          <li>Mixed Martial Arts</li>
+          <li>American Football</li>
+          <li>Volleyball</li>
+          <li>Badminton</li>
+          <li>Snooker</li>
+          <li>Ice Hockey</li>
+          <li>E Games</li>
+          <li>Futsal</li>
+          <li>Handball</li>
+          <li>Motor Sports</li>
+        </ul>
+      </div>
     </div>
   )
 }
